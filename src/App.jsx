f246d242166fb58feb7932800f2f14f9bf4ba9aa
FuryACE <sudhanshuvshekhar@gmail.com>
2022-06-27 17:12:52 +0530

    page structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,28 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+import Hero from "./components/Hero";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
+import Services from "./components/Services";
+import HireMe from "./components/HireMe";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+    <>
+      <div className=" min-h-screen">
+        <Hero />
+        <Skills />
+        <Projects />
+        <Experience />
+        <Services />
+        <HireMe />
+        <Footer />
+        <Navbar />
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
