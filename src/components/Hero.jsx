96133ff7cd0fc169f7279d19d61f2ae243d396cb
FuryACE <sudhanshuvshekhar@gmail.com>
2022-06-29 00:02:28 +0530

    added hero components features

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,14 @@ import classes from "./Hero.module.css";
 
 const Hero = () => {
   return (
-    <div className=" min-h-[35rem] relative px-16 py-36">
+    <div className=" min-h-[35rem] relative lg:px-20 py-36">
       <video className={classes.bgVideo} autoPlay loop muted>
         <source src={hero} type="video/mp4" />
       </video>
       <div className="absolute">
-        <h1 className={`text-5xl ${classes.heroTitle}`}>Hi, I'm Sudhanshu</h1>
-        <h2 className=" text-xl font-light py-5">A Full Stack Web Developer who likes to build amazing websites</h2>
+        <h1 className={`text-6xl ${classes.heroTitle}`}>Hi, I'm Sudhanshu</h1>
+        <h2 className={`text-2xl font-light py-7 ${classes.heroSubtitle}`}>A Full Stack Web Developer who likes to build amazing websites</h2>
+        <button className={`${classes.btn} bg-slate-50 block px-12 py-4 mt-6 text-black rounded-tl-none rounded-3xl items-center hover:rounded-none hover:bg-slate-900 hover:text-slate-100 duration-500`}>H I R E &nbsp;M E</button>
       </div>
     </div>
   );
