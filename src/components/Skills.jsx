ed0bd9077006e7d4b0d6b35214df6943d1f1471b
FuryACE <sudhanshuvshekhar@gmail.com>
2022-07-09 12:24:25 +0530

    added futuristic fonts

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Skills = () => {
   return (
-    <div>Skills</div>
-  )
-}
+    <>
+      <div className="flex flex-col items-center justify-center lg:h-[300px]">
+        <h1 className="text-3xl font-light pb-3 text-center border border-transparent border-b-emerald-200">
+          My Skills
+        </h1>
+        
+      </div>
+    </>
+  );
+};
 
-export default Skills
+export default Skills;
