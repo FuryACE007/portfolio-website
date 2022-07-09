4c83232e401d6bf7dae6717b986450d038e8457b
FuryACE <sudhanshuvshekhar@gmail.com>
2022-07-09 23:01:01 +0530

    fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import classes from './Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <div className={`fixed bottom-5 lg:left-[43rem] w-[350px] h-14 bg-white justify-center items-center text-center ${classes.glass}`}>
+    <div className={`fixed bottom-5 lg:left-[43rem] w-[350px] h-14 justify-center items-center text-center ${classes.glass}`}>
       
     </div>
   )
